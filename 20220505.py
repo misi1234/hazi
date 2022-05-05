26e35bde9d657bbe4bce8755f71976fe949772f3
misi1234 <pappmihaly1234567989@gmail.com>
2022-05-05 15:45:04 +0200

    hazi feladat 2022.05.05.

---
+++ 20220505.py
@@ -0,0 +1,41 @@
+
+try:
+    szam = int(input("Egész számokat kérek, kilépés 0-val: "))
+except:
+    pass
+
+szamok = []
+paros_szamok = []
+paratlan_szamok = []
+
+while szam != 0:
+    try:
+        szam = int(input('Egész számokat kérek, kilépés0-val: '))
+
+    except:
+        pass
+
+    if szam != 0:
+        szamok.append(szam)
+
+
+print(f'A számok összege: {sum(szamok)}')
+print(f'A számok átlaga: {sum(szamok) / len(szamok)}')
+
+for i in range(len(szamok)):
+    if szamok[i] % 2 == 0:
+        paros_szamok.append(szamok[i])
+    else:
+        paratlan_szamok.append(szamok[i])
+
+
+print(f"\n\nLegkisebb páros szám: {min(paros_szamok)}")
+print(f"Legnagyobb páros szám: {max(paros_szamok)}")
+print(f"Páros számok száma: {len(paros_szamok)}")
+print(f"Páros számok növekvő sorrendbe: {paros_szamok.sort()}")
+
+print("\n---------------------------------------------\n")
+print(f"Legkisebb páratlan szám: {min(paratlan_szamok)}")
+print(f"Legnagyobb páratlan szám: {max(paratlan_szamok)}")
+print(f"Páratlan számok száma: {len(paratlan_szamok)}")
+print(f"Páratlan számok: {paratlan_szamok }")
